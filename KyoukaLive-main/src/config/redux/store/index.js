acda407f30094e96b4338057fc0a723b987ec992
deset1ka <nastya.5sher@gmail.com>
2025-07-03 05:28:03 +0300

    Delete KyoukaLive-main directory

--- KyoukaLive-main/src/config/redux/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import watchReducer from "../watch/watch";
-
-export const store = configureStore({
-  reducer: {
-    watch: watchReducer,
-  },
-});
--- KyoukaLive-main/src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import Dashboard from "./dashboard";
-import Details from "./details";
-import Watch from "./watch";
-import Homepage from "./homepage";
-import Search from './search'
-
-export { Dashboard, Details, Watch, Homepage, Search };
